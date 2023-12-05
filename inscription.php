551cb4776085c5a0d73500e5bc2ea1516f11b2f7
Dezyop2 <wlebouvier@normandiewebschool.fr>
2023-12-05 12:20:39 +0100

    Create inscription.php

---
+++ inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+    <div class="form-floating mb-3">
+    <input type="" class="form-control" id="floatingInput" placeholder="">
+    <label for="floatingInput">Nom</label>
+    </div>
+
+    <div class="form-floating mb-3">
+    <input type="" class="form-control" id="floatingInput" placeholder="">
+    <label for="floatingInput">Prenom</label>
+    </div>
+
+    <div class="form-floating mb-3">
+    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+    <label for="floatingInput">Email address</label>
+    </div>
+    
+    <div class="form-floating">
+    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+    <label for="floatingPassword">Password</label>
+    </div>
+
+</body>
+</html>
