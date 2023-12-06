0e6a20455f23ca2300bd96238e16688a593edd7b
Gaspard Janty <144214357+GaspardJanty76@users.noreply.github.com>
2023-12-06 11:23:40 +0100

    one page

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+    <title>La Nuit Des Ambassadeurs</title>
 </head>
 <body>
-    
-</body>
+    <nav>
+      <div class="navbar">
+        <div class="logonav"><a href="#">La Nuit Des Ambassadeurs</a></div>
+        <ul class="menunav">
+          <li class="navbara"><a href="#Accueil">Accueil</a></li>
+          <li class="navbara"><a href="#LaNuitDesAmbassadeurs">La Nuit Des Ambassadeurs</a></li>
+          <li class="navbara"><a href="#Inscription">Inscription</a></li>
+        </ul>
+      </div>
+    </nav>
+    <section id="Accueil"></section>
+    <section id="LaNuitDesAmbassadeurs"></section>
+    <section id="Inscription"></section>
+    <div class="buttonup">
+      <a href="#Accueil"><svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><style>svg{fill:#35fcfc}</style><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg></a>
+    </div>
+  </body>
 </html>
