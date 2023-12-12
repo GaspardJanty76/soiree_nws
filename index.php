1032ef1eb9c64ec8678830eac9ede255ce6c8fc1
william lebouvier <wlebouvier@normandiewebschool.fr>
2023-12-12 09:04:20 +0100

    navbar v2

--- index.php
+++ index.php
@@ -10,19 +10,19 @@
 <body>
     <div class="navbar-color">
         <nav class="navbar">
-        <div class="container">
-            <a class="navbar-brand" href="#Accueil">Accueil</a>
-            <button class="navbar-toggler" id="toggleBtn">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav navbar-nav-right">
-                    <a class="nav-link" aria-current="page" href="#Info">Info</a>
-                    <a class="nav-link" href="#LaNuitDesAmbassadeurs">LDNA</a>
-                    <a class="nav-link" href="#Inscription">Inscription</a>
+            <div class="container">
+                <a class="navbar-brand" href="#Accueil">Accueil</a>
+                <button class="navbar-toggler" id="toggleBtn">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav navbar-nav-right">
+                        <a class="nav-link" aria-current="page" href="#Info">Info</a>
+                        <a class="nav-link" href="#LaNuitDesAmbassadeurs">LDNA</a>
+                        <a class="nav-link" href="#Inscription">Inscription</a>
+                    </div>
                 </div>
             </div>
-        </div>
         </nav>
 
         <div class="rectangles">
@@ -32,7 +32,7 @@
     </div>
 
     <div class="center-container">
-        <a href="#" class="inscription-btn">Inscription</a>
+        <a href="#Inscription" class="inscription-btn">Inscription</a>
     </div>
 
     <section id="Accueil">Accueil</section>
@@ -43,6 +43,6 @@
       <a href="#Accueil"><svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><style>svg{fill:#35fcfc}</style><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg></a>
     </div>
 
-    <script src="index.js"></script>
+    <script src="js/index.js"></script>
 </body>
 </html>
