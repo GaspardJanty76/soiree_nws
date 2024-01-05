bb32e0f1fde2f9109acc18936af18f678bd834a0
Dezyop2 <wlebouvier@normandiewebschool.fr>
2024-01-05 10:56:13 +0100

    v1.6

--- js/index.js
+++ js/index.js
@@ -18,3 +18,13 @@ var countdownInterval = setInterval(function() {
         document.getElementById("countdown").innerHTML = "EXPIRED";
     }
 }, 1000);
+
+// Fonction pour afficher le popup
+function showPopup() {
+    document.getElementById('overlay').style.display = 'flex';
+    }
+
+// Fonction pour fermer le popup
+function closePopup() {
+document.getElementById('overlay').style.display = 'none';
+}
